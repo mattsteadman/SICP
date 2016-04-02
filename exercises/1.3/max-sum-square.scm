781b05094482bafcfd958342d6471da6ffee677a
Matt Steadman <mmsteadman@gmail.com>
2016-04-02 16:15:37 -0400

    Create max-sum-square.scm

---
+++ exercises/1.3/max-sum-square.scm
@@ -0,0 +1,5 @@
+#lang scheme
+(define (max-square-sum x y z)
+        (cond ((and (< x y) (< x z)) (+ (* y y) (* z z)))
+              ((and (< y x) (< y z)) (+ (* x x) (* z z)))
+              ((and (< z x) (< z y)) (+ (* x x) (* y y)))))
